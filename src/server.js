d25b28e7c3583925d91a2c960e8f5035a7e4bb35
HaiK22TT <nchai.k22tt@kontum.udn.vn>
2026-05-10 03:27:24 +0700

    update project

--- src/server.js
+++ src/server.js
@@ -6,3 +6,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server đang chạy trên port: ${PORT}`);
 });
+
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  db: process.env.DB_NAME
+})
